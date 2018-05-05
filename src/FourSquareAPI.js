f7e36366c9729b56b92db4b05d563f4a363265fe
Renato Afonso <renato.roberto@gmail.com>
2018-05-05 02:42:30 +0100

    fix: deleted surplus file

--- src/FourSquareAPI.js
+++
@@ -1,5 +0,0 @@
-/* export const getAll = () => {
-    fetch(`https://api.foursquare.com/v2/venues/search?ll=38.7222524,-9.1393366&client_id=DKAUJUD2JV421KQCJENSDWHWBVALP1QWWMF3KKKP1CDWPPKE&client_secret=FBBD413XCHSBJFX5D2DWZUTVYBVOOWLSVM3G3SWEOJQPRA35&v=20180323&categoryId=4bf58dd8d48988d181941735&radius=5000&limit=50`)
-    .then(response => response.json())
-    .then(data => data.response.venues)
-        } */
